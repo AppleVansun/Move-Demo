8fb2efe63636b06311824c31ffb85cd252fe11c2
Vansun <vansun@tut.by>
2022-06-01 00:38:41 +0300

    Initial Commit

---
+++ Move Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Move Demo
+//
+//  Created by Ivan Grin on 1.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
